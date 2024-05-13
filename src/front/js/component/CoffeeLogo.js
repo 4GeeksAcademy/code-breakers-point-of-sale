af9a18808d3bd050cd113aa06be7a274b16be53a
finalfantasylover02 <109549032+finalfantasylover02@users.noreply.github.com>
2024-05-13 02:30:00 +0000

    added navbar

---
+++ src/front/js/component/CoffeeLogo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CoffeeLogo = ({ width, height }) => {
+    return <img src="../img/coffeeLogo.png" alt="Coffee Logo" style={{ width, height }} />;
+};
+
+export default CoffeeLogo;
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import CoffeeLogo from "./CoffeeLogo";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-backgroundColor: '#2db734'">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1"></span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/signIn">
-						<button className=""></button>
-					</Link>
-				</div>
-			</div>
-		</nav>
+		<AppBar position="fixed" sx={{ top: 0, backgroundColor: '#2db734' }}>
+            <Toolbar>
+                <Link to="/" style={{ flexGrow: 1, textDecoration: 'none', color: 'inherit' }}>
+                    <span className="navbar-brand mb-0 h1"> <CoffeeLogo width="100px" height="100px" /></span>
+					<Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'center' }}>
+            CODEFUSION CAFE
+          </Typography>
+                </Link>
+                <div className="ml-auto">
+                    <Link to="/signIn">
+                        <button className="btn btn-primary">Log out</button>
+                    </Link>
+                </div>
+            </Toolbar>
+        </AppBar>
 	);
 };
