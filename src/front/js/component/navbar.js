64f394d1c640dbf066a912cf4e71fe5b044321ea
finalfantasylover02 <109549032+finalfantasylover02@users.noreply.github.com>
2024-05-14 21:42:08 +0000

    centered and spaced everything

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -6,13 +6,13 @@ import CoffeeLogo from "./CoffeeLogo";
 export const Navbar = () => {
 	return (
 		<AppBar position="fixed" sx={{ top: 0, backgroundColor: '#2db734' }}>
-            <Toolbar>
-                <Link to="/" style={{ flexGrow: 1, textDecoration: 'none', color: 'inherit' }}>
-                    <span className="navbar-brand mb-0 h1"> <CoffeeLogo width="50px" height="50px" /> </span>  
-					<Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'center' }}>
-          CODEFUSION CAFE
-          </Typography>
+            <Toolbar style={{ justifyContent: 'space-between' }}>
+            <Link to="/" style={{ textDecoration: 'none', color: 'inherit', display: 'flex', alignItems: 'center' }}>
+                    <CoffeeLogo width="50px" height="50px" /> {/* Logo on the far left */}
                 </Link>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'center' }}>
+                    CODEFUSION CAFE {/* Centered text */}
+                </Typography>
                 <div className="ml-auto">
                     <Link to="/signIn">
                         <button className="btn btn-primary">EMPLOYEE ID</button>
