4fec93843bea7e20dbf96a6b6531bcee9ef5d8f0
finalfantasylover02 <109549032+finalfantasylover02@users.noreply.github.com>
2024-05-14 01:03:16 +0000

    added logo

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -8,14 +8,14 @@ export const Navbar = () => {
 		<AppBar position="fixed" sx={{ top: 0, backgroundColor: '#2db734' }}>
             <Toolbar>
                 <Link to="/" style={{ flexGrow: 1, textDecoration: 'none', color: 'inherit' }}>
-                    <span className="navbar-brand mb-0 h1"> <CoffeeLogo width="100px" height="100px" /></span>
+                    <span className="navbar-brand mb-0 h1"> <CoffeeLogo width="50px" height="50px" /> </span>  
 					<Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'center' }}>
-            CODEFUSION CAFE
+          CODEFUSION CAFE
           </Typography>
                 </Link>
                 <div className="ml-auto">
                     <Link to="/signIn">
-                        <button className="btn btn-primary">Log out</button>
+                        <button className="btn btn-primary">EMPLOYEE ID</button>
                     </Link>
                 </div>
             </Toolbar>
